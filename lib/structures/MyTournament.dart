f49d2f9886eab4c3f84cbbc2ca85143ea9f7645f
Jackhammer-95 <imgoinginside14@gmail.com>
2026-02-10 13:31:04 +0600

    Usericon added

--- lib/structures/MyTournament.dart
+++ lib/structures/MyTournament.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:gamesphere/TheProvider.dart';
+import 'package:gamesphere/widgets/ProfileDialog.dart';
+import 'package:provider/provider.dart';
 
 class MyTournament extends StatelessWidget {
   const MyTournament({super.key});
@@ -10,43 +13,95 @@ class MyTournament extends StatelessWidget {
   Widget build(BuildContext context) {
     final String? userUid = FirebaseAuth.instance.currentUser?.uid;
 
-    return Scaffold(
-      backgroundColor: const Color(0xFF0E0E12),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        surfaceTintColor: Colors.transparent,
-        centerTitle: false,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Text("MY  TOURNAMENTS", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900)),
-        ),
-        actions: [],
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('tournaments').where('admin_uid', isEqualTo: userUid).snapshots(),
-        builder: (context, snapshot){
-          if (snapshot.hasError) return Center(child: Text("Error: ${snapshot.error}"));
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator(color: Colors.blueAccent,));
-          }
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final bool loggedIn = snapshot.hasData && (snapshot.data != null);
+        final User? user = snapshot.data;
 
-          final docs = snapshot.data!.docs;
-
-          if(docs.isEmpty){
-            return _buildEmptyState();
-          }
-
-          return ListView.builder(
-            itemCount: docs.length,
-            padding: const EdgeInsets.all(16),
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index){
-              var data = docs[index].data() as Map<String, dynamic>;
-              return _buildTournamentCard(context, data);
+        return Scaffold(
+          backgroundColor: const Color(0xFF0E0E12),
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            surfaceTintColor: Colors.transparent,
+            centerTitle: false,
+            title: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("MY  TOURNAMENTS", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900)),
+            ),
+            actions: [
+              Padding(
+                padding: context.isMobile? const EdgeInsets.only(right: 12.0) : const EdgeInsets.only(right: 24.0),
+                child: loggedIn ? Consumer<UserProvider>(
+                  builder: (context, userProv, child) {
+                    return IconButton(
+                      icon: CircleAvatar(
+                        backgroundColor: Colors.white30,
+                        radius: 18,
+                        child: CircleAvatar(
+                          backgroundColor: const Color.fromARGB(255, 57, 92, 109),
+                          radius: 17,
+                          child: userProv.isLoading
+                            ? const SizedBox(
+                              width: 12.0,
+                              height: 12.0,
+                              child: CircularProgressIndicator(strokeWidth: 2.0, color: Colors.white,),
+                            )
+                            : Text(
+                              userProv.initial,
+                              style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                        ),
+                      ),
+                      onPressed: (){
+                        showProfileDialog(context, user!);
+                      },
+                    );
+                  }
+                )
+                : OutlinedButton.icon(
+                    onPressed: () {
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      side: const BorderSide(color: Color.fromARGB(77, 255, 255, 255)),
+                      padding: context.isMobile
+                          ? const EdgeInsets.symmetric(horizontal: 18, vertical: 9)
+                          : const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    ),
+                    icon: context.isMobile ? const Icon(Icons.login, size: 14) : const Icon(Icons.login, size: 18),
+                    label: context.isMobile? const Text("LOGIN", style: TextStyle(fontSize: 12.0)) : const Text("LOGIN"),
+                  ),
+            )
+            ],
+          ),
+          body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('tournaments').where('admin_uid', isEqualTo: userUid).snapshots(),
+            builder: (context, snapshot){
+              if (snapshot.hasError) return Center(child: Text("Error: ${snapshot.error}"));
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator(color: Colors.blueAccent,));
+              }
+        
+              final docs = snapshot.data!.docs;
+        
+              if(docs.isEmpty){
+                return _buildEmptyState();
+              }
+        
+              return ListView.builder(
+                itemCount: docs.length,
+                padding: const EdgeInsets.all(16),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index){
+                  var data = docs[index].data() as Map<String, dynamic>;
+                  return _buildTournamentCard(context, data);
+                },
+              );
             },
-          );
-        },
-      ),
+          ),
+        );
+      }
     );
   }
 
